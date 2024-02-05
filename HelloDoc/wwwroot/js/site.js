c88a9da2c4930a00ef0350afa617b67d2c69f3b9
pca45 <munavvar.popatiya@etatvasoft.com>
2024-02-05 11:07:31 +0530

    Add project files.

---
+++ HelloDoc/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+// Light Dark Mode Switching js
+var input = document.querySelector("#phone");
+var iti = window.intlTelInput(input, {
+    separateDialCode: true,
+    utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@16.0.3/build/js/utils.js",
+});
+
+// store the instance variable so we can access it in the console e.g. window.iti.getNumber()
+window.iti = iti;
+window.onload = () => {
+    const myModal = new bootstrap.Modal('#onload');
+    myModal.show();
+}
