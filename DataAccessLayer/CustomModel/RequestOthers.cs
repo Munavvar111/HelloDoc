9963dbc6013b21cbd1122f06a271775d7d49e44a
Munavvar Popatiya <tatva.dotnet.munavvarpopatiya@outlook.com>
2024-02-09 17:55:11 +0530

    4 request page

---
+++ DataAccessLayer/CustomModel/RequestOthers.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.CustomModel;
+
+public partial class RequestOthers
+{
+    [Key]
+    [StringLength(256)]
+    [Required(ErrorMessage = "Please Enter First Name")]
+    public string FirstNameOther { get; set; } = null!;
+
+    [StringLength(256)]
+    [Required(ErrorMessage ="Please Enter Last Name")]
+    public string LastNameOther { get; set; } = null!;
+
+    [StringLength(256)]
+    [Required(ErrorMessage = "Please Enter  Email")]
+    public string EmailOther { get; set; } = null!;
+
+    [StringLength(256)]
+    public string? Relation { get; set; }
+
+    [StringLength(256)]
+    public string? BusinessName { get; set; }
+
+    [StringLength(256)]
+    public string? HotelName { get; set; } 
+
+
+    [StringLength(256)]
+    [Required(ErrorMessage ="Please Enter FirstName Of Patient")]
+    public string FirstName { get; set; }= null!;
+
+    [StringLength(256)]
+    [Required(ErrorMessage ="Please Enter LastName Of Patient")]
+    public string LastName { get; set; }=   null!;
+
+    [StringLength(256)]
+    [Required(ErrorMessage ="Please Enter The Email Of Patient")]
+    public string Email { get; set; }=  null!;
+
+    [Required(ErrorMessage ="Please Enter The DOB Of the Patient")]
+    public DateOnly BirthDate { get; set; }
+
+    [Required(ErrorMessage = "Please Enter Street")]
+    public string Street { get; set; } = null!;
+
+    [Required(ErrorMessage = "Please Enter City")]
+    public string City { get; set; } = null!;
+
+    [Required(ErrorMessage = "Please Enter State")]
+    public string State { get; set; } = null!;
+
+    [Required(ErrorMessage = "Please Enter Zipcode")]
+    public string Zipcode { get; set; } = null!;
+
+
+}
--- HelloDoc/Controllers/RequestController.cs
+++ HelloDoc/Controllers/RequestController.cs
@@ -3,6 +3,7 @@ using DataAccessLayer.DataContext;
 using DataAccessLayer.DataModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol;
 
 namespace HalloDocPatient.Controllers
 {
@@ -26,10 +27,62 @@ namespace HalloDocPatient.Controllers
         {
             return View();
         }
-        
 
         [HttpPost]
-        public  IActionResult PatientRequest(RequestModel requestModel)
+        public IActionResult BusinessRequest(RequestOthers requestOther)
+        {
+            if (ModelState.IsValid)
+            {
+                Business business = new Business();
+                business.Name = requestOther.FirstNameOther;
+                business.City = requestOther.City;
+                business.Zipcode = requestOther.Zipcode;
+                business.Createddate = DateTime.Now;
+                business.Regionid = 1;
+
+                _context.Businesses.Add(business);
+                _context.SaveChanges();
+
+                Request request = new Request();
+                request.Firstname = requestOther.FirstNameOther;
+                request.Requesttypeid = 2;//Friend 
+                request.Lastname = requestOther.LastNameOther;
+                request.Email = requestOther.EmailOther;
+                request.Status = 1;//Unsigned
+                request.Createddate = DateTime.Now;
+
+                _context.Requests.Add(request);
+                _context.SaveChanges();
+
+                Requestbusiness requestbusiness = new Requestbusiness();
+                requestbusiness.Businessid = business.Businessid;
+                requestbusiness.Requestid = request.Requestid;
+
+                _context.Requestbusinesses.Add(requestbusiness);
+                _context.SaveChanges();
+
+
+                Requestclient requestclient = new Requestclient();
+                requestclient.Requestid = request.Requestid;
+                requestclient.Firstname = requestOther.FirstName;
+                requestclient.Lastname = requestOther.LastName;
+                requestclient.Email = requestOther.Email;
+                requestclient.Intdate = requestOther.BirthDate.Day;
+                requestclient.Intyear = requestOther.BirthDate.Year;
+                requestclient.Strmonth = requestOther.BirthDate.Month.ToString();
+
+
+                _context.Requestclients.Add(requestclient);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index", "dashboard");
+            }
+            return View(requestOther);
+        }
+
+
+        [HttpPost]
+        public  async Task<IActionResult> PatientRequest(RequestModel requestModel)
         {
              requestModel.Username = requestModel.Firstname+requestModel.Lastname;
             var modelStateErrors = this.ModelState.Values.SelectMany(m => m.Errors);
@@ -37,55 +90,89 @@ namespace HalloDocPatient.Controllers
             {
                 Guid guid = Guid.NewGuid();
                 string str = guid.ToString();
-                AspnetUser aspnetuser = new AspnetUser();
-                aspnetuser.Aspnetuserid = str;
-                aspnetuser.Email= requestModel.Email;
-                aspnetuser.Username = requestModel.Username;
-                _context.AspnetUsers.Add(aspnetuser);
-                _context.SaveChanges();
+                var email = _context.AspnetUsers.FirstOrDefault(u => u.Email == requestModel.Email);
+                if (email != null)
+                {
+                    var User= _context.Users.FirstOrDefault(u=>u.Email==requestModel.Email);
+                    Request request = new Request();
+                    request.Requesttypeid = 1;
+                    request.Userid =User.Userid;
+                    request.Firstname = requestModel.Firstname;
+                    request.Lastname = requestModel.Lastname;
+                    request.Email = requestModel.Email;
+                    request.Createddate = DateTime.Now;
+                    request.Status = 1;
+                    _context.Requests.Add(request);
+                    _context.SaveChanges();
 
-                User user= new User();
-                user.Email= requestModel.Email;
-                user.Aspnetuserid= aspnetuser.Aspnetuserid;
-                user.Firstname= requestModel.Firstname;
-                user.Lastname= requestModel.Lastname;   
-                user.Street=requestModel.Street;
-                user.City= requestModel.City;
-                user.State= requestModel.State;
-                user.Zipcode=requestModel.Zipcode;
-                user.Intyear = requestModel.BirthDate.Year;
-                user.Intdate = requestModel.BirthDate.Day;
-                user.Strmonth = requestModel.BirthDate.Month.ToString();
-                user.Createddate=DateTime.Now;
-                _context.Users.Add(user);
-                _context.SaveChanges();
+                    Requestclient requestclient = new Requestclient();
+                    requestclient.Requestid = request.Requestid;
+                    requestclient.Firstname = requestModel.Firstname;
+                    requestclient.Lastname = requestModel.Lastname;
+                    requestclient.State = requestModel.State;
+                    requestclient.Street = requestModel.Street;
+                    requestclient.City = requestModel.City;
+                    requestclient.Zipcode = requestModel.Zipcode;
 
-                Request request= new Request();
-                request.Requesttypeid = 1;
-                request.Userid=user.Userid;
-                request.Firstname=requestModel.Firstname;
-                request.Lastname=requestModel.Lastname; 
-                request.Email=requestModel.Email;
-                request.Createddate=DateTime.Now;
-                request.Status = 1;
-                _context.Requests.Add(request); 
-                _context.SaveChanges();
+                    _context.Requestclients.Add(requestclient);
+                    _context.SaveChanges();
+                    return RedirectToAction("Index", "Dashboard");
 
-                Requestclient requestclient= new Requestclient();   
-                requestclient.Requestid = request.Requestid;
-                requestclient.Firstname = requestModel.Firstname;
-                requestclient.Lastname = requestModel.Lastname;
-                requestclient.State= requestModel.State;
-                requestclient.Street= requestModel.Street;
-                requestclient.City= requestModel.City;
-                requestclient.Zipcode=requestModel.Zipcode;
-
-                _context.Requestclients.Add(requestclient); 
-                _context.SaveChanges(); 
+                }
+                else
+                {
 
 
+                    AspnetUser aspnetuser = new AspnetUser();
+                    aspnetuser.Aspnetuserid = str;
+                    aspnetuser.Email = requestModel.Email;
+                    aspnetuser.Username = requestModel.Username;
+                    _context.AspnetUsers.Add(aspnetuser);
+                    _context.SaveChanges();
 
-                return RedirectToAction("Index","Dashboard"); 
+                    User user = new User();
+                    user.Email = requestModel.Email;
+                    user.Aspnetuserid = aspnetuser.Aspnetuserid;
+                    user.Firstname = requestModel.Firstname;
+                    user.Lastname = requestModel.Lastname;
+                    user.Street = requestModel.Street;
+                    user.City = requestModel.City;
+                    user.State = requestModel.State;
+                    user.Zipcode = requestModel.Zipcode;
+                    user.Intyear = requestModel.BirthDate.Year;
+                    user.Intdate = requestModel.BirthDate.Day;
+                    user.Strmonth = requestModel.BirthDate.Month.ToString();
+                    user.Createddate = DateTime.Now;
+                    _context.Users.Add(user);
+                    _context.SaveChanges();
+
+                    Request request = new Request();
+                    request.Requesttypeid = 1;
+                    request.Userid = user.Userid;
+                    request.Firstname = requestModel.Firstname;
+                    request.Lastname = requestModel.Lastname;
+                    request.Email = requestModel.Email;
+                    request.Createddate = DateTime.Now;
+                    request.Status = 1;
+                    _context.Requests.Add(request);
+                    _context.SaveChanges();
+
+                    Requestclient requestclient = new Requestclient();
+                    requestclient.Requestid = request.Requestid;
+                    requestclient.Firstname = requestModel.Firstname;
+                    requestclient.Lastname = requestModel.Lastname;
+                    requestclient.State = requestModel.State;
+                    requestclient.Street = requestModel.Street;
+                    requestclient.City = requestModel.City;
+                    requestclient.Zipcode = requestModel.Zipcode;
+
+                    _context.Requestclients.Add(requestclient);
+                    _context.SaveChanges();
+
+
+
+                    return RedirectToAction("Index", "Dashboard");
+                }
             }
             else
             {
@@ -96,7 +183,7 @@ namespace HalloDocPatient.Controllers
         [HttpPost]
         public JsonResult CheckEmail([FromBody] string email)
         {
-            AspnetUser user = _context.AspnetUsers.SingleOrDefault(u => u.Email == email);
+            AspnetUser user = _context.AspnetUsers.FirstOrDefault(u => u.Email == email);
             bool isValid = user == null;
             return Json(isValid);
         }
@@ -105,11 +192,99 @@ namespace HalloDocPatient.Controllers
         {
             return View();
         }
-        public IActionResult ConceirgeRequest()
+
+        [HttpPost]
+        public IActionResult FriendRequest(RequestOthers request)
+        {
+            if(ModelState.IsValid)
+            {
+                Request friend=new Request();
+                friend.Requesttypeid = 2;//Friend 
+                friend.Firstname = request.FirstNameOther;
+                friend.Lastname = request.LastNameOther;
+                friend.Email = request.EmailOther;
+                friend.Status = 1;//Unsigned
+                friend.Relationname = request.Relation;
+                friend.Createddate = DateTime.Now;
+                _context.Requests.Add(friend);  
+                _context.SaveChanges();
+
+                Requestclient requestclient = new Requestclient();
+                requestclient.Requestid=friend.Requestid;
+                requestclient.Firstname=request.FirstName;
+                requestclient.Lastname= request.LastName;  
+                requestclient.Email= request.Email;
+                requestclient.Intdate = request.BirthDate.Day;
+                requestclient.Intyear=request.BirthDate.Year;
+                requestclient.Strmonth = request.BirthDate.Month.ToString();
+                requestclient.Street = request.Street;
+                requestclient.City = request.City;  
+                requestclient.State = request.State;    
+                requestclient.Zipcode = request.Zipcode;
+                _context.Requestclients.Add(requestclient);
+                _context.SaveChanges();
+
+
+            }
+            return View(request);
+        }
+            public IActionResult ConceirgeRequest()
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult ConceirgeRequest(RequestOthers requestOther)
+        {
+            if (ModelState.IsValid)
+            {
+                Concierge concierge = new Concierge();
+                concierge.Conciergename = requestOther.FirstNameOther;
+                concierge.City = requestOther.City;
+                concierge.State = requestOther.State;    
+                concierge.Street=requestOther.Street;
+                concierge.Zipcode=requestOther.Zipcode;
+                concierge.Createddate=DateTime.Now;
+                concierge.Regionid = 1;
 
+                _context.Concierges.Add(concierge);
+                _context.SaveChanges();
+
+                Request request = new Request();
+                request.Firstname = requestOther.FirstNameOther;
+                request.Requesttypeid = 2;//Friend 
+                request.Lastname = requestOther.LastNameOther;
+                request.Email = requestOther.EmailOther;
+                request.Status = 1;//Unsigned
+                request.Createddate = DateTime.Now;
+                
+                _context.Requests.Add(request);
+                _context.SaveChanges();
+
+                Requestconcierge requestconcierge= new Requestconcierge();
+                requestconcierge.Conciergeid = concierge.Conciergeid;
+                requestconcierge.Requestid=request.Requestid;
+
+                _context.Requestconcierges.Add(requestconcierge);
+                _context.SaveChanges();
+
+
+                Requestclient requestclient = new Requestclient();
+                requestclient.Requestid = request.Requestid;
+                requestclient.Firstname = requestOther.FirstName;
+                requestclient.Lastname = requestOther.LastName;
+                requestclient.Email = requestOther.Email;
+                requestclient.Intdate = requestOther.BirthDate.Day;
+                requestclient.Intyear = requestOther.BirthDate.Year;
+                requestclient.Strmonth = requestOther.BirthDate.Month.ToString();
+
+
+                _context.Requestclients.Add(requestclient);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index", "dashboard");
+            }
+            return View(requestOther);
+        }
 
     }
 }
