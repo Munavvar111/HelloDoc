53ee45e3e0d6d7d97c10aeee8d762bda508e3235
Munavvar Popatiya <tatva.dotnet.munavvarpopatiya@outlook.com>
2024-02-20 18:46:02 +0530

    admin

---
+++ DataAccessLayer/CustomModel/NewRequestTableVM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.CustomModel
+{
+    public class NewRequestTableVM
+    {
+        public string PatientName {  get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Requestor { get; set; }   
+
+        public DateTime ReqDate { get; set; } 
+
+        public string Address { get; set; } 
+
+        public string Notes { get; set; }   
+
+       public string Phone { get; set; }
+
+        public Int32 ReqTypeId { get; set; }  
+
+        public string Email { get; set; }   
+
+        public Int32 Id { get; set; }
+    }
+}
--- HelloDoc/Controllers/AdminController.cs
+++ HelloDoc/Controllers/AdminController.cs
@@ -1,6 +1,11 @@
 ﻿using BusinessLayer.Repository;
+using DataAccessLayer.CustomModel;
 using DataAccessLayer.DataContext;
+using DataAccessLayer.DataModels;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Org.BouncyCastle.Ocsp;
+using System.Linq;
 
 namespace HelloDoc.Controllers
 {
@@ -12,13 +17,160 @@ namespace HelloDoc.Controllers
         {
             _context = context;
         }
+
+        public IActionResult ProviderLocation()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        public IActionResult Provider()
+        {
+            return View();  
+        }
+
+        public IActionResult Parteners()
+        {
+            return View();
+        }
+        public IActionResult Records()
+        {
+            return View();
+        }
+
         public IActionResult Index()
+        {
+            
+            var request = from req in _context.Requests
+                          join reqclient in _context.Requestclients
+                          on req.Requestid equals reqclient.Requestid
+                          select new NewRequestTableVM
+                          {
+                              PatientName = reqclient.Firstname,
+                              Requestor = req.Firstname + " " + req.Lastname,
+                              DateOfBirth = new DateTime((int)reqclient.Intyear, int.Parse(reqclient.Strmonth), (int)reqclient.Intdate),
+                              ReqDate = req.Createddate,
+                              Phone = req.Phonenumber,
+                              Address = reqclient.City + reqclient.Zipcode,
+                              Notes = reqclient.Notes,
+                              ReqTypeId=req.Requesttypeid,
+                              Email=req.Email,
+                              Id=reqclient.Requestclientid
+,
+                          };
+            return View(request.ToList());
+        }
+
+        public IActionResult GetData()
         {
             var request = from req in _context.Requests
                           join reqclient in _context.Requestclients
                           on req.Requestid equals reqclient.Requestid
-                          select req ;
-            return View(request);
+                          select new NewRequestTableVM
+                          {
+                              PatientName = reqclient.Firstname,
+                              Requestor = req.Firstname + " " + req.Lastname,
+                              DateOfBirth = new DateTime((int)reqclient.Intyear, int.Parse(reqclient.Strmonth), (int)reqclient.Intdate),
+                              ReqDate = req.Createddate,
+                              Phone = req.Phonenumber,
+                              Address = reqclient.City + reqclient.Zipcode,
+                              Notes = reqclient.Notes
+,
+                          };
+            return Json(new {data=request.ToList()}, System.Web.Mvc.JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public IActionResult SearchPatient(string searchValue)
+        {
+            // Assuming _context is your DbContext
+            var filteredPatients = (from req in _context.Requests
+                                   join reqclient in _context.Requestclients
+                                   on req.Requestid equals reqclient.Requestid
+                                   select new NewRequestTableVM
+                                   {
+                                       PatientName = reqclient.Firstname,
+                                       Requestor = req.Firstname + " " + req.Lastname,
+                                       DateOfBirth = new DateTime((int)reqclient.Intyear, int.Parse(reqclient.Strmonth), (int)reqclient.Intdate),
+                                       ReqDate = req.Createddate,
+                                       Phone = req.Phonenumber,
+                                       Address = reqclient.City + reqclient.Zipcode,
+                                       Notes = reqclient.Notes
+         ,
+                                   }).Where(patient=>patient.PatientName.Contains(searchValue)).ToList();
+
+            return PartialView("NewTablePartial", filteredPatients);
+        }
+
+        public IActionResult Learning()
+        {
+            return View();
+        }
+
+
+        public IActionResult AjaxMethod()
+        {
+            try
+            {
+                var draw = HttpContext.Request.Form["draw"].FirstOrDefault();
+
+                // Skip number of Rows count  
+                var start = Request.Form["start"].FirstOrDefault();
+
+                // Paging Length 10,20  
+                var length = Request.Form["length"].FirstOrDefault();
+
+                // Sort Column Name  
+                var sortColumn = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
+
+                // Sort Column Direction (asc, desc)  
+                var sortColumnDirection = Request.Form["order[0][dir]"].FirstOrDefault();
+
+                // Search Value from (Search box)  
+                var searchValue = Request.Form["search[value]"].FirstOrDefault();
+
+                //Paging Size (10, 20, 50,100)  
+                int pageSize = length != null ? Convert.ToInt32(length) : 0;
+
+                int skip = start != null ? Convert.ToInt32(start) : 0;
+
+                int recordsTotal = 0;
+
+                // getting all Customer data  
+                var customerData = (from req in _context.Requests
+                                    join reqfile in _context.Requestclients
+                                    on req.Requestid equals reqfile.Requestid
+                                    select new
+                                    {
+                                        requestid=req.Requestid,
+                                        firstname=req.Firstname,
+                                        lastname=req.Lastname,
+                                        email=req.Email
+                                    }).ToList();
+                //Sorting  
+
+                //Search  
+                if (!string.IsNullOrEmpty(searchValue))
+                {
+                    customerData = customerData.Where(item => item.firstname.Contains(searchValue)).ToList();
+                }
+
+
+                //total number of rows counts   
+                recordsTotal = customerData.Count();
+                //Paging   
+                var data = customerData.Skip(skip).Take(pageSize).ToList();
+                //Returning Json Data  
+               return Json(new { draw = draw, recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data = data });
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
         }
     }
 }
